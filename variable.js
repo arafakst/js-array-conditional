c6dc6d5aa8d185a27af58cc5e9af166b277c1ae1
arafakst <arafakst@gmail.com>
2023-02-26 14:32:51 +0600

    first commit

---
+++ variable.js
@@ -0,0 +1,14 @@
+// number type variable
+
+var sunglassPrice = 130;
+
+//string type variable 
+var specialOneName = "Ariana Grande";
+
+//boolean type variable
+var isSingle = true;
+
+//two types of number: Integer---- 120,
+//float, decimal: 2.40,
+
+//
