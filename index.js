fe138364e1f9e6957ef67d841e9587641bcaf5c4
arafakst <arafakst@gmail.com>
2023-02-27 15:33:46 +0600

    array index

---
+++ index.js
@@ -0,0 +1,15 @@
+var numbers = [54, 23, 78, 56, 98, 47];
+
+
+//get elements by index
+//or
+var elements = numbers[1];
+//console.log(elements);
+//set element value by index 
+numbers[2] = 80;
+numbers[3] =60;
+//console.log(numbers);
+
+// find index of an element
+var positionIndex = numbers.indexOf(98);
+console.log(positionIndex);
