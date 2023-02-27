f3e03ff7e7533b687fd84eeabb120995c619d84a
arafakst <arafakst@gmail.com>
2023-02-27 18:50:43 +0600

    conditional - if/else

---
+++ conditional.js
@@ -0,0 +1,38 @@
+var iphonePrice = 79000;
+var myBudget = 95000;
+//if iphone price is less than my budget.I will bu this phone
+
+/**
+ * if iphone price less than my budget
+ *      i will buy the iphone
+ * 
+ * if(condition){
+ *  //will execute if the codition is true
+ * }
+ */
+
+if (iphonePrice < myBudget){
+    console.log('I phone kine futani korbo');
+}
+
+var chickenPrice = 500;
+var myMoney = 50;
+// if (chickenPrice <= myMoney){
+//     console.log('Murgii khabo haddi cibabo');
+// }
+// if (chickenPrice > myMoney){
+//     console.log('mashed potatto and lentils soup');
+// }
+
+/**
+ * if checken er price is less than my budget
+ * i will eat chken
+ * na hoi : else
+ */
+
+if (chickenPrice < myMoney){
+    console.log('I wull eat chicken');
+}
+else{
+    console.log('I will eat potato');
+}
