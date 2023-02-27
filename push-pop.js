8ae08bcb1ada884d0b87c59fc22928f471a33998
arafakst <arafakst@gmail.com>
2023-02-27 15:53:16 +0600

    add or remove element from array

---
+++ push-pop.js
@@ -0,0 +1,14 @@
+var numbers = [12, 14, 78, 56];
+//use push to add element to an array as the last element
+// numbers.push(63);
+//console.log(numbers);
+var friends = ['balam', 'habib', 'kalam'];
+friends.push('gelam');
+friends.push('pailam');
+// console.log(friends);
+
+//use pop to get last element
+console.log(numbers);
+var element = numbers.pop();
+console.log(numbers);
+console.log(element);
