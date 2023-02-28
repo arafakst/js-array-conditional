d264e795a3e49b02252468f7269266ec5497551e
arafakst <arafakst@gmail.com>
2023-02-28 17:27:08 +0600

    nested condition

---
+++ nested-condition.js
@@ -0,0 +1,21 @@
+var math = true;
+var geometry = true;
+var straightLine = false;
+if(math == true){
+    if(geometry == true){
+        if(geometry == true){
+            if (straightLine === true){
+
+            }
+            else{
+                console.log('Baka pothe Colba na')
+            }
+        }
+        else{
+            console.log('Pithagorous hote parba na')
+        }
+    }
+}
+else{
+
+}
