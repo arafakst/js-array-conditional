c431d6cf4140a4216a27aa877be03ad286435866
arafakst <arafakst@gmail.com>
2023-02-28 16:46:50 +0600

    multiple-condition-handle

---
+++ multiple-condition.js
@@ -0,0 +1,39 @@
+// var isGraduate = true;
+// var salary = 75000;
+// var cars = 1;
+
+// if(isGraduate === true){
+//     console.log('Eso Biye kore feli')
+// }
+// else{
+//     console.log('Tor koplae biye nai')
+// }
+
+// if(isGraduate === true && salary > 50000){
+//     console.log('Eso Biye kore feli')
+// }
+// else{
+//     console.log('Tor koplae biye nai')
+// }
+
+// if(isGraduate === true && salary > 50000 && cars >= 1){
+//     console.log('Eso Biye kore feli')
+// }
+// else{
+//     console.log('Tor koplae biye nai')
+// }
+
+var isGraduate = false;
+var salary = 31000;
+var cars = 1;
+
+if(isGraduate === true || salary > 50000 || cars >= 1){
+    console.log('Eso Prem Kori')
+}
+else{
+    console.log('Tomar bondhur number dao')
+}
+
+if (isGraduate === true && (salary > 50000 || cars >= 1)){
+
+}
