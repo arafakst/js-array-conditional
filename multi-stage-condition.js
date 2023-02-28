c27af275f6d08def6fc61e3f3eaad39265769536
arafakst <arafakst@gmail.com>
2023-02-28 16:59:46 +0600

    multi-stage-condition

---
+++ multi-stage-condition.js
@@ -0,0 +1,16 @@
+var myMoney = 68;
+var danishPrice = 45;
+var butterBread = 35;
+var toastBiscuit = 20;
+if(danishPrice < myMoney ){
+    console.log('Dan Dan dansh khabo')
+}
+else if (butterBread < myMoney){
+    console.log('Butter bon khamu')
+}
+else if (toastBiscuit < myMoney){
+    console.log('chubai chubai toast biscuit khamu')
+}
+else{
+    console.log('Khali ca sasther jonno valo')
+}
