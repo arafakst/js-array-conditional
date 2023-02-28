1aa0a4d8500969897112ae9dc2ed4f7865a7d3a9
arafakst <arafakst@gmail.com>
2023-02-28 17:45:13 +0600

    summary

---
+++ summary.js
@@ -0,0 +1,9 @@
+var destination = ['sajek', 'coxbazar', 'nepal', 'bali',];
+
+console.log(destination[2])
+destination[2] = 'thailan';
+var ondex = destination.indexOf('bali');
+
+destination.push('bhutan');
+destination.pop()
+
